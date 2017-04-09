dc25888a135ae6cb4e3f29557ac2c8663711877f
Jefzhang <jfzhang1993@gmail.com>
2017-04-09 21:59:29 +0200

    add index.php

---
+++ index.php
@@ -0,0 +1,114 @@
+<?php
+require('utils/utils.php');
+require('data/database.php');
+require('data/loginOut.php');
+$pageTitle = "sub";
+generateHTMLHeader($pageTitle, "css");
+session_name("tresor");
+session_start();
+if (!isset($_SESSION['initiated'])) {
+    session_regenerate_id();
+    $_SESSION['initiated'] = true;
+    $_SESSION['loggedIn'] = false;
+}
+
+$dbh = Database::connect();
+
+//check if this user already existe
+
+if (array_key_exists('todo', $_GET)) {
+    if ($_GET['todo']=='login') {
+        logIn($dbh);
+    } elseif ($_GET['todo']=='logout') {
+        logOut();
+    }
+}
+
+
+
+
+echo<<<END
+<div class="activity-modal " id="login-modal">
+  <div class="login-form login-modal-content acti-modal-animate container-fluid">
+    <form method="post" action="?todo=login" role="login">
+      <img src="images/logo.png"  class="img-responsive" alt="">
+      <p>
+      <input type="text" name="login" placeholder="Nom de votre binet" data-validation="required" data-validation-error-msg="Nom de binet invalide" class="form-control input-lg" required>
+      </p>
+      <p>
+      <input type="password" class="form-control input-lg" id="password" name="pass" placeholder="Mot de passe" data-validation="required" data-validation-erroe-msg="Mot de passe invalide">
+      </p>
+      <div class="pwstrength_viewport_progress"></div>
+      <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Se connecter</button>
+      <div>
+        <a href="http://localhost/project/register.php">Create account</a> or <a href="#">reset password</a>
+      </div>
+    </form>
+
+    <div class="form-links">
+      <a href="http://localhost/project/index.php">www.tresorfactory.com</a>
+    </div>
+  </div>
+</div>
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery-form-validator/2.3.26/jquery.form-validator.min.js"></script>
+
+END;
+
+if(isset($_SESSION['loggedIn'])){
+  generateMenu($_SESSION['loggedIn']);
+}
+else{
+  generateMenu(false);
+}
+if (isset($_SESSION['loggedIn'])&&$_SESSION["loggedIn"]) {         //already log in
+  //generateMenu();
+  if(isset($_GET['p'])){
+    //echo "<div class=\"container-fluid\">";
+    //echo "<div class=\"row\">";
+    $page = $_GET['p'];
+    if($page=='home')  require("content/home.php");
+    else if($page=='compte'){
+      echo Utilisateurs::getUtilisateur($dbh,$_SESSION['login']);
+    }
+    else if($page=='activity'){
+      require("content/activity.php");
+    }
+    //echo "</div";
+    //echo "</div";
+  }
+  else{
+    require("content/home.php");
+  }
+}else {
+  if(isset($_GET['p'])){
+    $page = $_GET['p'];
+    if($page=='home') require ("content/home.php");
+    else{
+      printLoginForm();
+    }
+  }
+  else{
+    require ("content/home.php");
+  }
+
+}
+
+
+generateHTMLFooter();
+
+
+?>
+<script>
+$.validate({
+});
+$("#seConnecter").on('click',function(e){
+ $("#login-modal").css('display','block');
+})
+var loginModal = document.getElementById('login-modal');
+window.onclick = function(event) {
+ if(event.target==loginModal){
+   loginModal.style.display="none";
+ }
+}
+</script>
