b7412f1cbd81e80ad1f9c2d6e90dacec6221ec58
boyuanhuang <bo-yuan.huang@polytechnique.edu>
2017-04-10 13:38:03 -0400

    Update compte.php

---
+++ content/compte.php
@@ -0,0 +1,29 @@
+<div class="w3-sidebar w3-light-grey w3-card-4 w3-animate-left" style="width:17%" id="mySidebar">
+    <div class="w3-bar w3-dark-grey">
+        <span class="w3-bar-item w3-padding-16">Content</span>
+        <button onclick="w3_close()" class="w3-bar-item w3-button w3-right w3-padding-16" title="close Sidebar">×</button>
+    </div>
+    <div class="w3-bar-block">
+        <a class="w3-bar-item w3-button w3-green" href="javascript:void(0)">Home</a>
+        <div class="w3-dropdown-hover">
+            <a class="w3-button" href="javascript:void(0)"> HISTORIQUE <i class="fa fa-caret-down"></i></a>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">2017-2018</a>
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">2016-2017</a>
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">2015-2016</a>
+            </div>
+        </div>
+
+        <div class="w3-dropdown-hover">
+            <a class="w3-button" href="javascript:void(0)"> EVOLUTION <i class="fa fa-caret-down"></i></a>
+            <div class="w3-dropdown-content w3-bar-block w3-card-4">
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">2017-2018</a>
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">2016-2017</a>
+                <a class="w3-bar-item w3-button" href="javascript:void(0)">2015-2016</a>
+            </div>
+        </div>
+        <a class="w3-bar-item w3-button" href="javascript:void(0)">About</a>
+        <a class="w3-bar-item w3-button" href="javascript:void(0)">Contact</a>
+
+    </div>
+</div>
